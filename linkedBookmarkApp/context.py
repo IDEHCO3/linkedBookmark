51fb335b0463fd9f5ca6fb41e46b387c003f5cdb
aluizio <aluizio777@gmail.com>
2017-03-02 15:15:55 -0300

    Added the context

--- linkedBookmarkApp/context.py
+++ linkedBookmarkApp/context.py
@@ -1,6 +1,38 @@
 
-context = {
-    "@context": {
+def addContextInHeader(url, response):
+    link = ' <'+url+'>; rel=\"http://www.w3.org/ns/json-ld#context\"; type=\"application/ld+json\"'
+    if "Link" not in response:
+        response['Link'] = link
+    else:
+        response['Link'] += ", " + link
+    return response
 
+LinkedBookmarkResourceContext = {
+    "@context": {
+        "name": {
+            "@id": "http://schema.org/name",
+            "@type": "http://schema.org/text"
+        },
+        "description": {
+            "@id": "http://schema.org/description",
+            "@type": "http://schema.org/text"
+        }
+    }
+}
+
+LinkedBookmarkItemResourceContext = {
+    "@context": {
+        "name": {
+            "@id": "http://schema.org/name",
+            "@type": "http://schema.org/text"
+        },
+        "description": {
+            "@id": "http://schema.org/description",
+            "@type": "http://schema.org/text"
+        },
+        "iri": {
+            "@id": "http://schema.org/url",
+            "@type": "@id"
+        }
     }
 }
--- linkedBookmarkApp/views.py
+++ linkedBookmarkApp/views.py
@@ -1,22 +1,69 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
 from linkedBookmarkApp.models import LinkedBookmarkResource, LinkedBookmarkItemResource
 from serializers import ResourceSerializer, ResourceItemSerializer
+from context import *
 # Create your views here.
 
 class ResourceList(generics.ListCreateAPIView):
     queryset = LinkedBookmarkResource.objects.all()
     serializer_class = ResourceSerializer
 
+    def get(self, request, *args, **kwargs):
+        response = super(ResourceList, self).get(request, *args, **kwargs)
+        response = addContextInHeader(reverse('linked-bookmark-app:resource-context', request=request), response)
+        return response
+
+    def options(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkResourceContext, status=200)
+
 class ResourceDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = LinkedBookmarkResource.objects.all()
     serializer_class = ResourceSerializer
 
+    def get(self, request, *args, **kwargs):
+        response = super(ResourceDetail, self).get(request, *args, **kwargs)
+        response = addContextInHeader(reverse('linked-bookmark-app:resource-context', request=request), response)
+        return response
+
+    def options(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkResourceContext, status=200)
+
+class ResourceContext(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkResourceContext, status=200)
 
 class ResourceItemList(generics.ListCreateAPIView):
     queryset = LinkedBookmarkItemResource.objects.all()
     serializer_class = ResourceItemSerializer
 
+    def get(self, request, *args, **kwargs):
+        response = super(ResourceItemList, self).get(request, *args, **kwargs)
+        response = addContextInHeader(reverse('linked-bookmark-app:resource-item-context', request=request), response)
+        return response
+
+    def options(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkItemResourceContext, status=200)
+
 class ResourceItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = LinkedBookmarkItemResource.objects.all()
     serializer_class = ResourceItemSerializer
+
+    def get(self, request, *args, **kwargs):
+        response = super(ResourceItemDetail, self).get(request, *args, **kwargs)
+        response = addContextInHeader(reverse('linked-bookmark-app:resource-item-context', request=request), response)
+        return response
+
+    def options(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkItemResourceContext, status=200)
+
+
+class ResourceItemContext(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response(LinkedBookmarkItemResourceContext, status=200)
 
